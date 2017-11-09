135e463b3445e1c4d2411e7d226a13a02964dd43
Богдан Игнатьев <submitthesatan666@gmail.com>
2017-11-09 17:03:52 +0200

    forget to remove sample service producer code for php agent guide. Now removing it

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Anotheria\MoskitoPHPAgent\producers\impl\ServiceOrientedProducer;
-
 class HomeController
 {
 
     public function index()
     {
-
-        $serviceProducer = new ServiceOrientedProducer('service', 'php', 'php');
-        $executionWatcher = $serviceProducer->getWatcher('sample-execution');
-        $executionWatcher->start();
-        // Some long executing stuff
-        sleep(3);
-        $executionWatcher->end();
-
         return view('index');
     }
 
